c4019b57cbb8e83f36bf0635b7b711b3c19ec5e6
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-01-24 23:41:51 +0100

    Goth

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,6 +3,24 @@ import { cn } from "../styles/utils";
 
 export type GameProps = ComponentProps<"div">;
 
+const assets = [
+	{
+		id: 1,
+		angry: "/angry-1.jpg",
+		happy: "/angry-1.jpg",
+	},
+	{
+		id: 2,
+		angry: "/angry-2.jpg",
+		happy: "/angry-2.jpg",
+	},
+	{
+		id: 3,
+		angry: "/angry-3.jpg",
+		happy: "/angry-3.jpg",
+	},
+];
+
 export function Game({ className, ...props }: GameProps) {
 	return (
 		<div className={cn("h-full w-full", className)} {...props}>
